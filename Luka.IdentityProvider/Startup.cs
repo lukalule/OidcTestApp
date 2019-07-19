8b64f80f70ed8a121724f63f8b0f405507600ec8
Your Name <you@example.com>
2019-07-19 15:28:04 +0200

    Identity Server 4 set up and in memory data for clients, users and identityResources add quickstart UI

--- Luka.IdentityProvider/Startup.cs
+++ Luka.IdentityProvider/Startup.cs
@@ -15,8 +15,13 @@ namespace Luka.IdentityProvider
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential();
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryClients(Config.GetClients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,10 +32,9 @@ namespace Luka.IdentityProvider
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
